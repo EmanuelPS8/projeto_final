1f5cce30f05c692a8ef9bd3d3384b9b04b048bd9
EmanuelPS8 <97064304+EmanuelPS8@users.noreply.github.com>
2025-06-23 08:45:24 -0300

    config.py para render

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py (correto)
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:TFiwsudwtErlsvfYKIOntCLpFYnIoqMq@caboose.proxy.rlwy.net:16652/railway'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:TFiwsudwtErlsvfYKIOntCLpFYnIoqMq@caboose.proxy.rlwy.net:16652/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
