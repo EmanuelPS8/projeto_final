04254424ed0b8135769a0ebc56ab28dbabb21384
EmanuelPS8 <97064304+EmanuelPS8@users.noreply.github.com>
2025-06-23 08:41:36 -0300

    ajuste no endereço do railway

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py (correto)
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:1234@localhost:3306/projeto_web'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:TFiwsudwtErlsvfYKIOntCLpFYnIoqMq@caboose.proxy.rlwy.net:16652/railway'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
